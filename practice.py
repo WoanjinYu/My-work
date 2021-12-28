18c443125e41e69682c00a532ecf6cab82f5fb3b
Wonajin yu <yuwanjin70@gamil.com>
2021-12-28 13:46:53 +0200

    first commit

---
+++ practice.py
@@ -0,0 +1,17 @@
+from random import *
+
+# print(random()) # 0.0~ 1.0
+# print(random()* 10) # 0.0 ~ 10.0
+# print(int(random()* 10)) #0 ~ 10
+
+# print(int(random()* 10)) #0 ~ 10
+
+# print(int(random()* 10)) #0 ~ 10
+# print(int(random()* 10 )+1) #1 ~ 11
+
+print(int (random() *45 +1)) #1 ~45까지의 임의의수
+print(int (random() *45 +1)) #1 ~45까지의 임의의수
+print(int (random() *45 +1)) #1 ~45까지의 임의의수
+print(int (random() *45 +1)) #1 ~45까지의 임의의수
+print(int (random() *45 +1)) #1 ~45까지의 임의의수
+print(int (random() *45 +1)) #1 ~45까지의 임의의수
