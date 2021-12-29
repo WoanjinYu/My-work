08ad04538955686aab8c94d9df647e6f3cd1594f
WoanjinYu <yuwanjin70@gmail.com>
2021-12-29 11:42:07 +0200

    git commit -m "second commit"

--- pygame_basic/3_namin_sprite.py
+++ pygame_basic/3_namin_sprite.py
@@ -11,7 +11,17 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Wonajin's game") # game name
 
 #배경 이미지 불러오기
-background = pygame.image.load("C:/Users/Woanjin/Desktop/Python workspace/pygame_basic/커비화면.png")
+background = pygame.image.load("C:/Users/Woanjin/Desktop/Python workspace/pygame_basic/background.png")
+
+#  캐릭터(스프라이트) 불러오기
+character = pygame.image.load("C:/Users/Woanjin/Desktop/Python workspace/pygame_basic/character.png")
+character_size = character.get_rect().size #이미지의 크기를 구해옴
+character_width = character_size[0] #캐릭터의 가로크기
+character_height = character_size[1] #캐릭터의 세로크기
+character_x_pos = (screen_width / 2)-(character_width/2) #화면 가로의 절반 크기에 해달하는 곳에 위치
+character_y_pos = screen_height - character_height #화면 세로크기 가장 아래에 해당하는 곳에 위치
+
+
 
 #event roop
 running = True #is it continuing?
@@ -21,6 +31,8 @@ while running:
             running = False # 게임이 진행중이 아님
     #screen.fill((0, 0, 255))
     screen.blit(background, (0, 0)) # 배경 그리기
+
+    screen.blit(character, (character_x_pos, character_y_pos)) #캐릭터 
 
     pygame.display.update() #게임화면을 다시 그리기!
 
